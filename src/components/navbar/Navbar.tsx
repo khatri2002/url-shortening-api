ff9425e6d41c815f922de63fd0b589b551517d7d
Jay Khatri <jay.khatri@oorjan.com>
2024-12-19 17:36:51 +0530

    feat(sticky position of navbar implemented)

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import classNames from "classnames";
 import { RxHamburgerMenu } from "react-icons/rx";
@@ -10,43 +10,59 @@ import { Logo } from "../../assets/images";
 
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState(false);
+  const [navbarAppear, setNavbarAppear] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 400 && !navbarAppear) setNavbarAppear(true);
+      if (window.scrollY < 10 && navbarAppear) setNavbarAppear(false);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [navbarAppear]);
 
   return (
-    <nav className={commonStyles.container}>
-      <div className={styles.logo}>
-        <img src={Logo} alt="logo" />
-      </div>
-      <div
-        className={classNames({
-          [styles.navItems]: true,
-          [styles.show]: showNavbar,
-        })}
-      >
-        <ul className={styles.nav}>
-          <li className={styles.item}>
-            <span>Features</span>
-          </li>
-          <li className={styles.item}>
-            <span>Pricing</span>
-          </li>
-          <li className={styles.item}>
-            <span>Resources</span>
-          </li>
-        </ul>
-        <span className={styles.separator} />
-        <ul className={styles.action}>
-          <li className={styles.item}>
-            <button className={styles.login}>Login</button>
-          </li>
-          <li className={styles.item}>
-            <button className={styles.signUp}>Sign Up</button>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.iconContainer}>
-        <button onClick={() => setShowNavbar((prev) => !prev)}>
-          <RxHamburgerMenu className={styles.icon} />
-        </button>
+    <nav className={classNames({ [styles.appear]: navbarAppear })}>
+      <div className={`${commonStyles.container} ${styles.container}`}>
+        <div className={styles.logo}>
+          <img src={Logo} alt="logo" />
+        </div>
+        <div
+          className={classNames({
+            [styles.navItems]: true,
+            [styles.show]: showNavbar,
+          })}
+        >
+          <ul className={styles.nav}>
+            <li className={styles.item}>
+              <span>Features</span>
+            </li>
+            <li className={styles.item}>
+              <span>Pricing</span>
+            </li>
+            <li className={styles.item}>
+              <span>Resources</span>
+            </li>
+          </ul>
+          <span className={styles.separator} />
+          <ul className={styles.action}>
+            <li className={styles.item}>
+              <button className={styles.login}>Login</button>
+            </li>
+            <li className={styles.item}>
+              <button className={styles.signUp}>Sign Up</button>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.iconContainer}>
+          <button onClick={() => setShowNavbar((prev) => !prev)}>
+            <RxHamburgerMenu className={styles.icon} />
+          </button>
+        </div>
       </div>
     </nav>
   );
